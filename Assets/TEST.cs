737053a992210ea670060032bc86229bb287e125
mioQAQ <chim05450@gmail.com>
2019-11-01 00:44:33 +0800

    Initial commit

---
+++ Assets/TEST.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class TEST : MonoBehaviour
+{
+    [SerializeField]
+		public GameObject C1,C2;
+
+    void Start()
+    {
+        if(C1.transform.position==C2.transform.position){
+			Debug.Log("TRUE");
+			}else Debug.Log("FALSE");
+
+
+
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# LittleRed
+ UnityGamePractice
