1e7de0f5b772be48946e4203d4e4a244e21f1d26
Pan0519 <pan19860519@gmail.com>
2019-11-02 00:46:23 +0800

    Add New

---
+++ Assets/Scripts/GameControllerNew.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerNew : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    bool moveFlag;
+
+    [SerializeField]
+    bool FinishFlag;
+
+    [SerializeField]
+    GameObject FloorGroup;
+
+    List<Transform> TerrainList = new List<Transform>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!moveFlag || !FinishFlag)
+        {
+            return;
+        }
+
+
+    }
+
+    private float step
+    {
+        get
+        {
+            return speed * Time.deltaTime;
+        }
+    }
+}
